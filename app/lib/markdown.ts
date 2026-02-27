4b9e38ed13b2e8ba081a0a8345b901bce813b7f6
xuhong <cxh710200711@gmail.com>
2026-02-27 09:37:40 +0800

    sanitize markdown html output

--- app/lib/markdown.ts
+++ app/lib/markdown.ts
@@ -1,5 +1,13 @@
 import { marked } from "marked";
+import sanitizeHtml from "sanitize-html";
 
 export function renderMarkdown(markdown: string): string {
-  return marked.parse(markdown, { gfm: true }) as string;
+  const html = marked.parse(markdown, { gfm: true }) as string;
+  return sanitizeHtml(html, {
+    allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img", "h1", "h2", "h3"]),
+    allowedAttributes: {
+      a: ["href", "name", "target", "rel"],
+      img: ["src", "alt", "title", "width", "height"],
+    },
+  });
 }
